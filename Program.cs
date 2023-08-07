6e67e7a0e752f17df4a17634a79cdb55eec1d646
Shakthy <shakthy.r@metamation.com>
2023-08-07 14:10:49 +0530

    completed T01

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿namespace _1 {
+   internal class Program {
+      static void Main (string[] args) {
+         Console.WriteLine ("Enter a number :");
+         if (int.TryParse (Console.ReadLine (), out int num)) {
+            Console.WriteLine ("Do you want convert to (B)inary or (H)exadecimal?");
+            switch (Console.ReadKey ().Key) {
+               case (ConsoleKey.B):
+                  Console.WriteLine ();
+                  Console.WriteLine (Convert.ToString (num, 2));
+                  break;
+               case (ConsoleKey.H):
+                  Console.WriteLine ();
+                  Console.WriteLine ($"{num:X}");
+                  break;
+               default:
+                  Console.WriteLine ("Enter a valid number");
+                  break;
+            }
+         }
+      }
+   }
+}
