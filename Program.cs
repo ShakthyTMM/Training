a69dbf611ff4dd2a3bb1a565cfbfccbf50d2deea
Shakthy <shakthy.r@metamation.com>
2023-08-09 17:01:07 +0530

    Completed T06

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a text: ");
+string name = Console.ReadLine ();
+var origi = name.ToLower ().Replace (" ", "").ToCharArray ();
+var rev = name.ToLower ().Replace (" ", "").ToCharArray ();
+Array.Reverse (rev);
+if (Enumerable.SequenceEqual (origi, rev)) Console.WriteLine ("It is a palindrome"); else Console.WriteLine ("It is not a palindrome");
