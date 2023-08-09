d37001053851865938ff2fdf2ec61218a684e8c3
Shakthy <shakthy.r@metamation.com>
2023-08-09 16:57:59 +0530

    Completed T05

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a number: ");
+int num = int.Parse (Console.ReadLine ());
+int i;
+if (num == 1)
+   Console.WriteLine ("1 is neither Prime or Composite");
+else {
+   for (i = 2; i <= num / 2; i++) {
+      if (num % i == 0) {
+         Console.WriteLine ("The number is not a prime number");
+         break;
+      }
+   }
+   if (i > num / 2) Console.WriteLine ("The number is a prime number");
+}
