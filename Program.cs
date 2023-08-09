ccf76e61b7c901d735eadf5fac3f9559e4eccc12
Shakthy <shakthy.r@metamation.com>
2023-08-09 16:49:56 +0530

    Completed T03

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter number of terms: ");
+int n = int.Parse (Console.ReadLine ());
+int num = 0, sum = 1, val = 0;
+if (n == 1) {
+   Console.Write ("0");
+} else Console.Write ("0 1 ");
+for (int i = 1; i <= n - 2; i++) {
+   if (n > 2) {
+      val = num + sum;
+      Console.Write (val + " ");
+      num = sum;
+      sum = val;
+   }
+}
