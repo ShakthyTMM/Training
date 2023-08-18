e320cf30012e1481184d461af03a986f27caf079
Shakthy <shakthy.r@metamation.com>
2023-08-18 16:52:30 +0530

    Completed T09

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter no of lines: ");
+int n = int.Parse (Console.ReadLine ());
+int space = n;
+for (int i = 1; i <= (n * 2) + 1; i += 2) {
+   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", space)) + string.Concat (Enumerable.Repeat ("*", i)));
+   space--;
+}
+int blank = 1;
+for (int j = (n * 2) - 1; j >= 1; j -= 2) {
+   Console.WriteLine (string.Concat (Enumerable.Repeat (" ", blank)) + string.Concat (Enumerable.Repeat ("*", j)));
+   blank++;
+}
