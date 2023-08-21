e9345e4978d1ce2e68658c29cb8df4ba4a761ff0
Shakthy <shakthy.r@metamation.com>
2023-08-21 12:55:38 +0530

    Completed T10

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a number: ");
+int num = int.Parse (Console.ReadLine ());
+int original = num;
+int n = 0;
+while (num > 0) {
+   int rem = num % 10;
+   n = (10 * n) + rem;
+   num /= 10;
+}
+Console.WriteLine (n);
+if (original == n) Console.WriteLine ("It is a palindrome");
+else Console.WriteLine ("It is not a palindrome");
