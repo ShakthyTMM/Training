0018d7d5b20f15395ace21adffbaef9014e23455
Shakthy <shakthy.r@metamation.com>
2023-08-21 17:00:40 +0530

    Completed T12

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter number of rows: ");
+int rows = int.Parse (Console.ReadLine ());
+int val = 0;
+for (int i = 0; i < rows; i++) {
+   for (int blank = 0; blank <= rows - i; blank++)
+      Console.Write ("   ");
+   for (int j = 0; j <= i; j++) {
+      if (i == 0 || j == 0 || i == j) val = 1;
+      else {
+         val = val * (i - j + 1) / j;
+      }
+      Console.Write (val + "     ");
+   }
+   Console.WriteLine ();
+}
+
