ec218aed0d5f3ee7bef96bc0cddb3c64a635a864
Shakthy <shakthy.r@metamation.com>
2023-08-21 14:55:42 +0530

    Completed T11

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a number: ");
+int num = int.Parse (Console.ReadLine ());
+int sum = 0;
+while (num > 0) {
+   int n = num % 10;
+   sum += n;
+   num /= 10;
+}
+if (sum > 10) {
+   int t = sum % 10;
+   sum /= 10;
+   int m = t + sum;
+   Console.WriteLine (m);
+} else Console.WriteLine (sum);
