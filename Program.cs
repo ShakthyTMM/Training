805e606cd639c00316f1fc3b35a6e8928ed28d4c
Shakthy <shakthy.r@metamation.com>
2023-08-22 12:26:23 +0530

    Completed A01

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿while (true) {
+   int guess = new Random ().Next (1, 100);
+   string userguess;
+   int val = 0, count = 0;
+   Console.WriteLine ("Guess a number between 1 and 100");
+   while (val != guess) {
+      userguess = Console.ReadLine ();
+      if ((!int.TryParse (userguess, out val) || val < 1 || val > 100)) {
+         Console.WriteLine ("Invalid Number");
+         continue;
+      }
+      if (val > guess) Console.WriteLine ("Guess is High");
+      else if (val != guess) Console.WriteLine ("Guess is Low");
+      count++;
+   }
+   Console.WriteLine (val + " is the correct number");
+   Console.WriteLine ("Total number of attempts " + count);
+   Console.WriteLine (" Press ENTER to play again");
+   if (Console.ReadKey ().Key != ConsoleKey.Enter) break;
+   else
+      continue;
+}
