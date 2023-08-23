435d78e9e3538452da03b04eb620421d57f15640
Shakthy <shakthy.r@metamation.com>
2023-08-23 17:31:32 +0530

    Modified T03

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿Console.WriteLine ("Enter number of terms: ");
-int n = int.Parse (Console.ReadLine ());
-int num = 0, sum = 1, val = 0;
-if (n == 1) {
-   Console.Write ("0");
-} else Console.Write ("0 1 ");
-for (int i = 1; i <= n - 2; i++) {
-   if (n > 2) {
-      val = num + sum;
-      Console.Write (val + " ");
-      num = sum;
-      sum = val;
+﻿Console.WriteLine ("Enter number of terms : ");
+int length = int.Parse (Console.ReadLine () ?? "");
+List<int> series = new List<int> ();
+int a = 0, b = 1, count = 1;
+Fibonacci (a, b, count, length);
+void Fibonacci (int a, int b, int count, int length) {
+   if (count <= length) {
+      Console.Write (a + " ");
+      series.Add (a);
+      Fibonacci (b, a + b, count + 1, length);
    }
+}
+Console.WriteLine ();
+foreach (var item in series) {
+   Console.Write (item + " ");
 }
