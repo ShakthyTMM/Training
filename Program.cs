a1d5729ffa67736417cd80bab054f3e90f87bf16
Shakthy <shakthy.r@metamation.com>
2023-08-24 17:03:43 +0530

    Completed T16

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a word: ");
+string String = Console.ReadLine () ?? "";
+Console.Write ("ABECEDARIAN WORD: ");
+Console.WriteLine (Check (String));
+char[] Check (string String) {
+   char[] Sortword = String.ToArray ();
+   Array.Sort (Sortword);
+   for (int i = 1; i < Sortword.Length; i++) {
+      if (Sortword[0] != Sortword[i]) return (Sortword);
+   }
+   char[] empty = new char[0];
+   return empty;
+}
