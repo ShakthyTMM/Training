1ee8aef67e984a88b2310ba8cd4e552cd1cdd5bf
Shakthy <shakthy.r@metamation.com>
2023-08-24 15:20:30 +0530

    Completed T14

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a string: ");
+string String = Console.ReadLine () ?? "";
+char[] Chars = String.ToCharArray ();
+if (String != null) {
+   char[] Resultstring = new char[100];
+   for (int i = 0; i < Chars.Length - 1; i++) {
+      if (Chars[i] == Chars[i + 1]) {
+         if (i == 0) Resultstring[i] = Chars[i];
+         continue;
+      }
+      if (Chars.Length > 2) { Resultstring[i] = Chars[i + 1]; continue; } else if (Chars.Length == 2) { Resultstring[i] = Chars[i]; Resultstring[i + 1] = Chars[i + 1]; break; }
+   }
+   Resultstring[0] = Chars[0];
+   for (int j = 0; j < Resultstring.Length; j++) Console.Write (Resultstring[j]);
+} else Console.WriteLine ("Empty String");
