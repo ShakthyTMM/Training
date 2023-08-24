1fc2d97d707d6f8d5fa12c6880a8b44ceb43e96a
Shakthy <shakthy.r@metamation.com>
2023-08-24 15:45:34 +0530

    Completed T15

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿Console.WriteLine ("Enter a string: ");
+string String = Console.ReadLine () ?? "";
+char[] Isogram = String.ToCharArray ();
+bool result = CheckIsogram (Isogram, String);
+if (result == true) Console.WriteLine ("The string is an Isogram");
+else Console.WriteLine ("The string is not an Isogram");
+bool CheckIsogram (char[] Isogram, string String) {
+   for (int i = 0; i < String.Length - 2; i++) {
+      for (int j = i + 1; j < String.Length; j++) {
+         if (Isogram[i] == Isogram[j]) return false;
+         else continue;
+
+      }
+   }
+   return true;
+}
