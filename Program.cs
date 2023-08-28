28293f5b6f9010c83cb029a4743c60d37582d67c
Shakthy <shakthy.r@metamation.com>
2023-08-28 12:54:33 +0530

    Modified T01

--- Program.cs
+++ Program.cs
@@ -1,23 +1,22 @@
-﻿namespace _1 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Enter a number :");
-         if (int.TryParse (Console.ReadLine (), out int num)) {
-            Console.WriteLine ("Do you want convert to (B)inary or (H)exadecimal?");
-            switch (Console.ReadKey ().Key) {
-               case (ConsoleKey.B):
-                  Console.WriteLine ();
-                  Console.WriteLine (Convert.ToString (num, 2));
-                  break;
-               case (ConsoleKey.H):
-                  Console.WriteLine ();
-                  Console.WriteLine ($"{num:X}");
-                  break;
-               default:
-                  Console.WriteLine ("Enter a valid number");
-                  break;
-            }
-         }
+﻿using static System.Console;
+while (true) {
+   WriteLine ("\nEnter a number :");
+   if (int.TryParse (Console.ReadLine (), out int num)) {
+      WriteLine ("Do you want convert to (B)inary or (H)exadecimal?");
+      switch (ReadKey ().Key) {
+         case (ConsoleKey.B):
+            WriteLine ("\n" + Convert.ToString (num, 2));
+            break;
+         case (ConsoleKey.H):
+            WriteLine ($"\n{num:X}");
+            break;
+         default:
+            WriteLine ("Enter a valid number");
+            break;
       }
    }
+   WriteLine ("Do you want to convert again? (Y)es or (No)");
+   if (ReadKey ().Key == ConsoleKey.Y) continue;
+   else break;
 }
+      
