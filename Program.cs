387150fb8a9ff6007f555e9bc032a162d58c4834
Shakthy <shakthy.r@metamation.com>
2023-08-28 16:39:22 +0530

    Modified T05

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
-﻿Console.WriteLine ("Enter a number: ");
-int num = int.Parse (Console.ReadLine ());
+﻿//Program to check prime number
+Console.WriteLine ("Enter a number: ");
+int.TryParse (Console.ReadLine () ?? "", out int num);
 int i;
 if (num == 1)
    Console.WriteLine ("1 is neither Prime or Composite");
 else {
-   for (i = 2; i <= num / 2; i++) {
+   int Limit = num / 2; 
+   for (i = 2; i <= Limit; i++) {
       if (num % i == 0) {
          Console.WriteLine ("The number is not a prime number");
          break;
       }
    }
-   if (i > num / 2) Console.WriteLine ("The number is a prime number");
+   if (i > Limit) Console.WriteLine ("The number is a prime number");
 }
