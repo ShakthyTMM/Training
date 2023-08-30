ad10e65c3acee6dac6af43794ea555695c076b07
Shakthy <shakthy.r@metamation.com>
2023-08-30 09:23:55 +0530

    Modified T18.1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,25 @@
-﻿using System.Linq;
-internal class Program {
+﻿internal class Program {
    private static void Main (string[] args) {
-      List<int> list = new List<int> ();
-      IEnumerable<int> a = Enumerable.Range (1, 10000000);
-      foreach (var i in a) {
-         string length = Convert.ToString (i);
-         var number = i;
-         double sum = 0;
-         for (int j = 1; j <= length.Length; j++) {
-            var rem = number % 10;
-            sum = sum + Math.Pow (rem, length.Length);
-            number /= 10;
-         }
-         if (sum == i) {
-            list.Add (i);
-         }
-      }
-      string temp = String.Join ("", args);
-      int count = Convert.ToInt32 (temp);
-      Console.WriteLine (list.ElementAt (count - 1));
+      if (int.TryParse (args[0], out int count) && args.Length == 1 && Convert.ToInt32 (args[0]) > 0)
+         GetArmstrong (count);
+      else Console.WriteLine ("Enter a valid number");
    }
-
+   static void GetArmstrong (int count) {
+      int index = 0;
+      double sum = 0;
+      var number = 0;
+      while (count > index) {
+         number++;
+         sum = 0;
+         var check = number;
+         string length = Convert.ToString (number);
+         for (int j = 1; j <= length.Length; j++) {
+            var rem = check % 10;
+            sum = sum + Math.Pow (rem, length.Length);
+            check /= 10;
+         }
+         if (sum == number) index++;
+      }
+      Console.WriteLine ($"The {count}th Armstrong Number is : {sum}");
+   }
 }
