c688aef5d97ecc8b41f6eee71f7d52f242b79995
Shakthy <shakthy.r@metamation.com>
2023-08-30 12:15:30 +0530

    Completed T21

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿//Program to Swap two Numbers
+Console.Write ("Enter the First Number: ");
+int.TryParse (Console.ReadLine () ?? "", out int Number1);
+Console.Write ("Enter the Second Number: ");
+int.TryParse (Console.ReadLine () ?? "", out int Number2);
+SwapNumbers (Number1, Number2);
+
+void SwapNumbers (int a, int b) {
+   (a, b) = (b, a);
+   Console.WriteLine ($"The Swapped Numbers: \nThe First Number is {a} \nThe Second Number is {b}");
+}
