dc8f8013f601bacf07fed46b5fcf600a81499612
Shakthy <shakthy.r@metamation.com>
2023-08-30 11:59:29 +0530

    Completed T20

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿//Program to  DISPLAY THE INDIVIDUAL DIGITS OF A GIVEN NUMBER 
+Console.WriteLine ("Enter a number: ");
+if (double.TryParse (Console.ReadLine () ?? "", out double Input)) {
+   string Output = Input.ToString ();
+   GetNumbers (Output);
+} else Console.WriteLine ("Enter a valid number");
+void GetNumbers (string Number) {
+   List<string> Integral = new List<string> ();
+   List<string> Decimal = new List<string> ();
+   foreach (var ch in Number) {
+      if (ch.ToString () != ".") {
+         Integral.Add (ch.ToString ());
+         continue;
+      } else if (ch.ToString () == ".") {
+         int Length = Number.IndexOf (ch) + 1;
+         for (int i = Length; i < Number.Length; i++) Decimal.Add (Number[i].ToString ());
+      }
+      break;
+   }
+   Console.Write ("The Integral Part is : ");
+   Integral.ForEach (num => Console.Write (num + " "));
+   Console.Write ("\nThe Fractional Part is : ");
+   Decimal.ForEach (num => Console.Write (num + " "));
+}
