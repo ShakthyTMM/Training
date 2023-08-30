3552cd0bf36174cf75fbebc7748f65935b9533d7
Shakthy <shakthy.r@metamation.com>
2023-08-30 12:02:14 +0530

    Completed T19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿//Program to Find Factorial of a Number
+Console.WriteLine ("Enter a number: ");
+double.TryParse (Console.ReadLine (), out double fact);
+Console.WriteLine (Factorial (fact));
+double Factorial (double n) {
+   if (n != 0)
+      return n * Factorial (n - 1);
+   return 1;
+}
+
