66bb13cde38e3941707392040183c9d576f1e050
Shakthy <shakthy.r@metamation.com>
2023-08-31 14:25:38 +0530

    Modified T14

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
-﻿Console.WriteLine ("Enter a string: ");
-string String = Console.ReadLine () ?? "";
-char[] Chars = String.ToCharArray ();
-if (String != null) {
-   char[] Resultstring = new char[100];
-   for (int i = 0; i < Chars.Length - 1; i++) {
-      if (Chars[i] == Chars[i + 1]) {
-         if (i == 0) Resultstring[i] = Chars[i];
-         continue;
-      }
-      if (Chars.Length > 2) { Resultstring[i] = Chars[i + 1]; continue; } else if (Chars.Length == 2) { Resultstring[i] = Chars[i]; Resultstring[i + 1] = Chars[i + 1]; break; }
+﻿//Program to display Reduced String
+Console.WriteLine ("Enter a string: ");
+var Input = Console.ReadLine () ?? "";
+List<string> Output = new List<string> ();
+(int, int) CountChar (string Input) {
+   int c = 0, i = 0;
+   for (i = 0; i < Input.Length - 1; i++) {
+      if (Input[i] == Input[i + 1]) c++;
+      else break;
    }
-   Resultstring[0] = Chars[0];
-   for (int j = 0; j < Resultstring.Length; j++) Console.Write (Resultstring[j]);
-} else Console.WriteLine ("Empty String");
+   return (c, i);
+}
+while (Input.Length != 0) {
+   (int count, int j) = CountChar (Input);
+   if (count % 2 == 0) Output.Add (Input[j].ToString ());
+   Input = Input.Substring (j + 1);
+}
+Output.ForEach (Console.Write);
