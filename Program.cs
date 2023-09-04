4779a61c26f8dc7e4f49b6d4545aa20b8297f7e6
Shakthy <shakthy.r@metamation.com>
2023-09-04 14:33:21 +0530

    Modified T17

--- Program.cs
+++ Program.cs
@@ -1,16 +1,19 @@
 ﻿// Program to Print String Permutations
 using static System.Console;
+int count = 0;
 string answer = "";
 Write ("Enter the string : ");
 string s = ReadLine () ?? "";
 if (s.Length > 0 && s.All (Char.IsLetter)) {
    WriteLine ("\nAll Possible Strings are : ");
    Permute (s, answer);
+   Write ($"\nNumber of Possible Permutations: {count}\n");
 } else WriteLine ("Enter Valid Input");
 
 void Permute (string s, string answer) {
    if (s.Length == 0) {
-      WriteLine (answer + "  ");
+      WriteLine (answer);
+      count++;
       return;
    }
    for (int i = 0; i < s.Length; i++)
