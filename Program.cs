818d25c2982db2d5281f83dd16e79af521a09323
Shakthy <shakthy.r@metamation.com>
2023-09-04 09:36:25 +0530

    Completed T17

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// Program to Print String Permutations
+string answer = "";
+Console.Write ("Enter the string : ");
+string s = Console.ReadLine () ?? "";
+if (s.Length > 0 && s.All (Char.IsLetter)) {
+   Console.WriteLine ("\nAll possible strings are : ");
+   permute (s, answer);
+} else Console.WriteLine ("Enter Valid Input");
+void permute (String s, String answer) {
+   if (s.Length == 0) {
+      Console.WriteLine (answer + "  ");
+      return;
+   }
+   for (int i = 0; i < s.Length; i++) {
+      char ch = s[i];
+      String left_substr = s.Substring (0, i);
+      String right_substr = s.Substring (i + 1);
+      String rest = left_substr + right_substr;
+      permute (rest, answer + ch);
+   }
+}
