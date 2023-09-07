90dbaf159c3c187b2cecbd797774b52a1f7d2c8d
Shakthy <shakthy.r@metamation.com>
2023-09-07 11:00:58 +0530

    Completed A02.3 (Program to Guess Secret Number from LSB to MSB)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// Program to Guess Secret Number (FROM LSB to MSB)
+using static System.Console;
+
+int Remainder = 0, Divisor = 2, Number = 0, Exp = 0;
+for (int tries = 0; tries <= 7; tries++) {
+   Write ($"Is the remainder {Remainder} when divided by {Divisor} (Y/N)? ");
+   switch (ReadKey ().Key) {
+      case ConsoleKey.Y:
+         Number += (int)(0 * Math.Pow (2, Exp));
+         Exp++;
+         Divisor = (int)Math.Pow (2, Exp + 1);
+         break;
+      case ConsoleKey.N:
+         Remainder += Divisor / 2;
+         Number += (int)(1 * Math.Pow (2, Exp));
+         Exp++;
+         Divisor = (int)Math.Pow (2, Exp + 1);
+         break;
+   }
+   WriteLine ();
+}
