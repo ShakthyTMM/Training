cb44368f69038b186345555c546e69dc283aa272
Shakthy <shakthy.r@metamation.com>
2023-09-08 16:01:34 +0530

    Modified T12 - Program to Print Pascal's Triangle

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
-﻿Console.WriteLine ("Enter number of rows: ");
-int rows = int.Parse (Console.ReadLine ());
+﻿// Program to Print Pascal's Triangle
+using static System.Console;
+WriteLine ("Enter number of rows: ");
+int rows = int.Parse (ReadLine () ?? "");
 int val = 0;
 for (int i = 0; i < rows; i++) {
    for (int blank = 0; blank <= rows - i; blank++)
-      Console.Write ("   ");
+      Write ("   ");
    for (int j = 0; j <= i; j++) {
       if (i == 0 || j == 0 || i == j) val = 1;
       else {
          val = val * (i - j + 1) / j;
       }
-      Console.Write (val + "     ");
+      Write (val + "     ");
    }
-   Console.WriteLine ();
+   WriteLine ();
 }
 
