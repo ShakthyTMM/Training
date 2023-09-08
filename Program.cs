d03c2d55807758100edff64810428163b4ae8265
Shakthy <shakthy.r@metamation.com>
2023-09-08 12:17:32 +0530

    Modified A02.3

--- Program.cs
+++ Program.cs
@@ -4,18 +4,12 @@ using static System.Console;
 int Remainder = 0, Divisor = 2, Number = 0, Exp = 0;
 for (int tries = 0; tries <= 7; tries++) {
    Write ($"Is the remainder {Remainder} when divided by {Divisor} (Y/N)? ");
-   switch (ReadKey ().Key) {
-      case ConsoleKey.Y:
-         Number += (int)(0 * Math.Pow (2, Exp));
-         Exp++;
-         Divisor = (int)Math.Pow (2, Exp + 1);
-         break;
-      case ConsoleKey.N:
-         Remainder += Divisor / 2;
-         Number += (int)(1 * Math.Pow (2, Exp));
-         Exp++;
-         Divisor = (int)Math.Pow (2, Exp + 1);
-         break;
+   if (ReadKey ().Key == ConsoleKey.N) {
+      Remainder += Divisor / 2;
+      Number += (int)(1 * Math.Pow (2, Exp));
    }
+   Exp++;
+   Divisor = (int)Math.Pow (2, Exp + 1);
    WriteLine ();
 }
+WriteLine ($"\nThe Secret Number is : {Number}");
