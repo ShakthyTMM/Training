84135fdbfae7b0dde897e1e8b7e7e79c0a2de226
Shakthy <shakthy.r@metamation.com>
2023-09-08 15:24:27 +0530

    Modified T11- Program to print digital root

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿Console.WriteLine ("Enter a number: ");
-int num = int.Parse (Console.ReadLine ());
+﻿//Program to Print Digital Root
+using static System.Console;
+WriteLine ("Enter a number: ");
+int num = int.Parse (ReadLine ()?? "");
 int sum = 0;
 while (num > 0) {
    int n = num % 10;
@@ -10,5 +12,5 @@ if (sum > 10) {
    int t = sum % 10;
    sum /= 10;
    int m = t + sum;
-   Console.WriteLine (m);
-} else Console.WriteLine (sum);
+   WriteLine (m);
+} else WriteLine (sum);
