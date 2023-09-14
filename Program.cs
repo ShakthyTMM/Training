2998648380496cd29a9287da9eaa5d2fef224c9f
Shakthy <shakthy.r@metamation.com>
2023-09-14 18:08:53 +0530

    A03

--- Program.cs
+++ Program.cs
@@ -3,22 +3,15 @@ using static System.Console;
 using System.IO;
 
 string[] words = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words-shuffle.txt");
-char[] letters = { 'U', 'X', 'A', 'L', 'T', 'N', 'E' };
+char[] letters = { 'E', 'I', 'S', 'A', 'R', 'N', 'T' };
 var resultList = new List<string> ();
-foreach (var word in words) {
-   int count = 0;
-   for (int i = 0; i < word.Length; i++) {
-      if (!letters.Contains (word[i])) {
-         count = 0;
-         break;
-      } else count++;
-   }
-   if (count >= 4 && word.Contains (letters[0]))
-      resultList.Add (word);
-}
+foreach (var word in words.Where(x=> x.Length >= 4 
+                               && x.Contains (letters[0]) 
+                               && letters.All (x.Contains))) 
+resultList.Add (word);
 resultList = resultList.OrderBy (x => x).OrderByDescending (x => x.Length).ToList ();
 int point, score = 0;
-resultList.Remove (Pangram (resultList));
+resultList = Pangram (resultList);
 foreach (var ch in resultList) {
    ForegroundColor = ConsoleColor.White;
    if (ch.Length == 4) {
@@ -30,14 +23,14 @@ foreach (var ch in resultList) {
    }
 }
 WriteLine ($"----\n{score} total");
-string Pangram (List<string> list) {
-   foreach (var ch in list) {
-      if (ch.Length > 4 && letters.All (ch.Contains)) {
-         ForegroundColor = ConsoleColor.Green;
-         WriteLine ($"{point = ch.Length + 7}. {ch}");
-         score += ch.Length + 7;
-         return ch;
-      }
+List<string> Pangram (List<string> list) {
+   var pangrams = new List<string> ();
+   foreach (var ch in list.Where (x => x.Length > 4 && letters.All (x.Contains))) {
+      pangrams.Add (ch);
+      ForegroundColor = ConsoleColor.Green;
+      WriteLine ($"{point = ch.Length + 7}. {ch}");
+      list.Remove (ch);
+      score += ch.Length + 7;
    }
-   return "";
+   return list;
 }
