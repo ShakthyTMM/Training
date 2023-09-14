c1ea88a3d2b342d337c3a9a3bba5d74428c2fc4f
Shakthy <shakthy.r@metamation.com>
2023-09-14 15:34:43 +0530

    Modified T22

--- Program.cs
+++ Program.cs
@@ -1,20 +1,16 @@
 ﻿//Program to Swap Indices of the Random Number Generated
-Console.WriteLine ("The Number Generated: ");
+using static System.Console;
+WriteLine ("The Number Generated: ");
 Random random = new Random ();
-int Number = random.Next ();
-Console.WriteLine (Number);
-Console.Write ("Enter the Indices to be Swapped: ");
-int.TryParse (Console.ReadLine () ?? "", out int index1);
-int.TryParse (Console.ReadLine () ?? "", out int index2);
-SwapIndices (Number, index1, index2);
+string num = random.Next ().ToString ();
+WriteLine (num);
+Write ("Enter the Indices to be Swapped: ");
+int.TryParse (ReadLine () ?? "", out int index1);
+int.TryParse (ReadLine () ?? "", out int index2);
+SwapIndices (num, index1, index2);
 
-void SwapIndices (int num, int a, int b) {
-   string Input = num.ToString ();
-   int[] NumberArr = new int[Input.Length];
-   for (int j = 0; j < Input.Length; j++) {
-      NumberArr[j] = int.Parse (Input[j].ToString ());
-   }
-   (NumberArr[a], NumberArr[b]) = (NumberArr[b], NumberArr[a]);
-   for (int i = 0; i < NumberArr.Length; i++) Console.Write (NumberArr[i]);
-
+void SwapIndices (string Input, int a, int b) {
+   char[] result = Input.ToCharArray ();
+   (result[a], result[b]) = (result[b], result[a]);
+   for (int j = 0; j < result.Length; j++) Write (result[j]);
 }
