6042c7b9559812d3bc3046575dd7c54b59f6d6b2
Shakthy <shakthy.r@metamation.com>
2023-09-14 09:47:38 +0530

    Modified T09

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿//Program to Print Diamond
 Console.WriteLine ("Enter no of lines: ");
-int n = int.Parse (Console.ReadLine ()?? "");
+int n = int.Parse (Console.ReadLine () ?? "");
 int space = n;
-for (int i = 1; i <= (n * 2) + 1; i += 2) {
+int condition = (n * 2) + 1;
+for (int i = 1; i <= condition; i += 2) {
    Console.WriteLine (string.Concat (Enumerable.Repeat (" ", space)) + string.Concat (Enumerable.Repeat ("*", i)));
    space--;
 }
 int blank = 1;
-for (int j = (n * 2) - 1; j >= 1; j -= 2) {
+for (int j = condition - 2; j >= 1; j -= 2) {
    Console.WriteLine (string.Concat (Enumerable.Repeat (" ", blank)) + string.Concat (Enumerable.Repeat ("*", j)));
    blank++;
 }
