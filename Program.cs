2674cf07f5c5a3fab0cda43555086a4aed5385ce
Shakthy <shakthy.r@metamation.com>
2023-09-14 16:07:19 +0530

    Factorial Iterative Method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// Program to implement Factorial in a iterative method
+using static System.Console;
+WriteLine ("Enter a number: ");
+int.TryParse (ReadLine (), out var fact);
+WriteLine (Factorial (fact));
+int Factorial (int fact) {
+   int f = 1;
+   for (int i = 1; i <= fact; i++)
+      f *= i;
+   return f;
+}
