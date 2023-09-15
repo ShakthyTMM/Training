b0f523c5afbdd9efc65ff2c8e489b818c6e3ccd5
Shakthy <shakthy.r@metamation.com>
2023-09-15 16:13:37 +0530

    Modified A03

--- Program.cs
+++ Program.cs
@@ -1,36 +1,23 @@
 ﻿// Program to develop Spell Bee game
 using static System.Console;
-using System.IO;
 
-string[] words = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words-shuffle.txt");
 char[] letters = { 'E', 'I', 'S', 'A', 'R', 'N', 'T' };
-var resultList = new List<string> ();
-foreach (var word in words.Where(x=> x.Length >= 4 
-                               && x.Contains (letters[0]) 
-                               && letters.All (x.Contains))) 
-resultList.Add (word);
-resultList = resultList.OrderBy (x => x).OrderByDescending (x => x.Length).ToList ();
-int point, score = 0;
-resultList = Pangram (resultList);
-foreach (var ch in resultList) {
-   ForegroundColor = ConsoleColor.White;
-   if (ch.Length == 4) {
-      WriteLine ($"{point = 1,2}. {ch}");
-      score++;
-   } else {
-      WriteLine ($"{point = ch.Length,2}. {ch}");
-      score += ch.Length;
-   }
+int score = 0;
+var validWords = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words-shuffle.txt")
+                              .Where (x => x.Length >= 4
+                               && x.Contains (letters[0])
+                               && x.All (letters.Contains)).Select (GetScore)
+                               .OrderBy (x => x.Item3).OrderByDescending (x => x.Item1).ToList ();
+foreach (var word in validWords) {
+   if (word.Item2) ForegroundColor = ConsoleColor.Green;
+   WriteLine ($"{word.Item1,5} {word.Item3}");
+   ResetColor ();
 }
-WriteLine ($"----\n{score} total");
-List<string> Pangram (List<string> list) {
-   var pangrams = new List<string> ();
-   foreach (var ch in list.Where (x => x.Length > 4 && letters.All (x.Contains))) {
-      pangrams.Add (ch);
-      ForegroundColor = ConsoleColor.Green;
-      WriteLine ($"{point = ch.Length + 7}. {ch}");
-      list.Remove (ch);
-      score += ch.Length + 7;
-   }
-   return list;
+WriteLine ($"-----------------\n{score,5} TOTAL");
+(int, bool, string) GetScore (string word) {
+   int point;
+   bool pangram = letters.All (word.Contains);
+   point = (word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0);
+   score += point;
+   return (point, pangram, word);
 }
