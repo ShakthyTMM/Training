1a40cafeb6d965ea3d9205f1b3b2b081deea737d
Shakthy <shakthy.r@metamation.com>
2023-09-15 16:33:40 +0530

    Modified T15

--- Program.cs
+++ Program.cs
@@ -1,16 +1,10 @@
-﻿Console.WriteLine ("Enter a string: ");
-string String = Console.ReadLine () ?? "";
-char[] Isogram = String.ToCharArray ();
-bool result = CheckIsogram (Isogram, String);
-if (result == true) Console.WriteLine ("The string is an Isogram");
-else Console.WriteLine ("The string is not an Isogram");
-bool CheckIsogram (char[] Isogram, string String) {
-   for (int i = 0; i < String.Length - 2; i++) {
-      for (int j = i + 1; j < String.Length; j++) {
-         if (Isogram[i] == Isogram[j]) return false;
-         else continue;
-
-      }
-   }
-   return true;
+﻿//Program to Check for ISOGRAM
+using static System.Console;
+WriteLine ("Enter a string: ");
+string isogram = ReadLine () ?? "";
+bool result = CheckIsogram (isogram);
+WriteLine ("The String is " + (result == true ? "" : "not ") + "an Isogram");
+bool CheckIsogram (string Isogram) {
+   return Isogram.Distinct ().Count () == Isogram.Length;
 }
+
