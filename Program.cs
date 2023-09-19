760db7101b994c6cca3849768cd859cccdab8262
Shakthy <shakthy.r@metamation.com>
2023-09-19 16:45:37 +0530

    Modified A03: Spell Bee

--- Program.cs
+++ Program.cs
@@ -1,24 +1,24 @@
 ﻿// Program to develop Spell Bee game
 using static System.Console;
 
-char[] Letters = { 'E', 'I', 'S', 'A', 'R', 'N', 'T' };
-int TotalScore = 0;
+char[] letters = { 'E', 'I', 'S', 'A', 'R', 'N', 'T' };
+int totalScore = 0;
 var validWords = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words-shuffle.txt")
                               .Where (x => x.Length >= 4
-                               && x.Contains (Letters[0])
-                               && x.All (Letters.Contains)).Select (GetScore)
-                               .OrderBy (x => x.Pangram).OrderByDescending (x => x.Score).ThenBy (x => x).ToList ();
+                               && x.Contains (letters[0])
+                               && x.All (letters.Contains)).Select (GetScore)
+                               .OrderByDescending (x => x.Score).ThenBy (x => x).ToList ();
 
-foreach (var Word in validWords) {
-   if (Word.Pangram) ForegroundColor = ConsoleColor.Green;
-   WriteLine ($"{Word.Score,5} {Word.Word}");
+foreach (var word in validWords) {
+   if (word.Pangram) ForegroundColor = ConsoleColor.Green;
+   WriteLine ($"{word.Score,5} {word.Word}");
    ResetColor ();
 }
-WriteLine ($"-----------------\n{TotalScore,5} TOTAL");
-(int Score, bool Pangram, string Word) GetScore (string Word) {
-   int Score;
-   bool Pangram = Letters.All (Word.Contains);
-   Score = (Word.Length > 4 ? Word.Length : 1) + (Pangram ? 7 : 0);
-   TotalScore += Score;
-   return (Score, Pangram, Word);
+WriteLine ($"-----------------\n{totalScore,5} TOTAL SCORE");
+(int Score, bool Pangram, string Word) GetScore (string word) {
+   int score;
+   bool pangram = letters.All (word.Contains);
+   score = (word.Length > 4 ? word.Length : 1) + (pangram ? 7 : 0);
+   totalScore += score;
+   return (score, pangram, word);
 }
