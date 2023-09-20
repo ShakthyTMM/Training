8167bd7075e252e8fc93d5a6d51525c5c3109283
Shakthy <shakthy.r@metamation.com>
2023-09-20 13:04:26 +0530

    A04: Spell Bee[Frequency table]

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// program to build a frequency table
+using static System.Console;
+Dictionary<char, int> frequency = new ();
+var words = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words.txt")
+                .SelectMany (x => x);
+foreach (var ch in words) {
+   if (frequency.TryGetValue (ch, out var count)) frequency[ch] = count + 1;
+   else frequency.Add (ch, count = 1);
+}
+foreach (var word in frequency.OrderByDescending (x => x.Value).Take (7))
+   WriteLine (word);
