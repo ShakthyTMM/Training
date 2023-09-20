9ace65172f33f0bb716b3cfe6716e4d35f40e97e
Shakthy <shakthy.r@metamation.com>
2023-09-20 17:34:13 +0530

    T23: Voting Contest

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// Program for Voting Contest
+using System.Diagnostics.Metrics;
+using static System.Console;
+WriteLine ("Give your vote(s): ");
+string votes = ReadLine ().ToLower ().Replace (" ", "");
+if (votes.All (Char.IsLetter)) {
+   var vote = getWinner (votes);
+   string key = vote.Char.ToString ();
+   WriteLine ((vote.Char != ' ' ? $"The Winner is ({key} or {key.ToUpper ()}) with {vote.Counter} vote(s)" : "No votes were counted"));
+} else Write ("Enter a valid vote");
+
+(char Char, int Counter) getWinner (string votes) {
+   if (votes != "") {
+      var vote = votes.GroupBy (x => x)
+            .Select (x => new { Char = x.Key, counter = x.Count () })
+            .OrderByDescending (x => x.counter).FirstOrDefault ();
+      return (vote.Char, vote.counter);
+   } else return (' ', 0);
+}
