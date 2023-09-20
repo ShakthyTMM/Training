736134df18791d2fb88c6868ee4ab6087d45b9e0
Shakthy <shakthy.r@metamation.com>
2023-09-20 14:31:00 +0530

    Modified A04 : spell bee frequency table

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var words = File.ReadAllLines ("C:/Users/shakthy.r/Downloads/words.txt")
                 .SelectMany (x => x);
 foreach (var ch in words) {
    if (frequency.TryGetValue (ch, out var count)) frequency[ch] = count + 1;
-   else frequency.Add (ch, count = 1);
+   else frequency.Add (ch, 1);
 }
 foreach (var word in frequency.OrderByDescending (x => x.Value).Take (7))
    WriteLine (word);
