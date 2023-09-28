4216110e24817dde99b77de29c5139e358d88569
Shakthy <shakthy.r@metamation.com>
2023-09-28 12:37:37 +0530

    T23:Voting contest(Modified)

--- Program.cs
+++ Program.cs
@@ -3,22 +3,17 @@
 //Program to get votes in a string and return the winner with maximum number of votes
 //Sample Input :Helloworld
 //Sample Output :L, 3 => Winner and votes 
-using System.Diagnostics.Metrics;
 using static System.Console;
 WriteLine ("Give your vote(s): ");
 string votes = ReadLine ().ToLower ().Replace (" ", "");
-if (votes != "" && votes.All (Char.IsLetter)) {
-   var vote = getWinner (votes);
-   string key = vote.Char.ToString ();
-   WriteLine ($"The Winner is ({key} or {key.ToUpper ()}) with {vote.Counter} vote(s)");
+if (!string.IsNullOrEmpty (votes) && votes.All (Char.IsLetter)) {
+   var vote = GetWinner (votes);
+   WriteLine ($"The Winner is {vote.Candidate} with {vote.Votes} vote(s)");
 } else Write ("Enter a valid vote");
 
 ///<summary>Function getWinner to return the winner with number of votes </summary>
 ///<param name="votes">votes given as string</param>
-(char Char, int Counter) getWinner (string votes) {
-   var vote = votes.GroupBy (x => x)
-                 .Select (x => (Char: x.Key, counter: x.Count ()))
-                 .OrderByDescending (x => x.counter).FirstOrDefault ();
-   return (vote.Char, vote.counter);
-
+(char Candidate, int Votes) GetWinner (string votes) {
+   var vote = votes.GroupBy (x => x).Select (x => (candidate: x.Key, votes: x.Count ())).OrderByDescending (x => x.votes).FirstOrDefault ();
+   return (vote.candidate, vote.votes);
 }
