bfec31ab4c546afcd34c580b59f27e2e5f2732e8
Shakthy <shakthy.r@metamation.com>
2023-09-28 16:58:40 +0530

    T24:Chocolate Wrappers

--- Program.cs
+++ Program.cs
@@ -5,22 +5,38 @@
 //Sample Input :X = 15, P = 4, W = 3
 // Sample Output :C = 4, X = 3, W = 1
 using static System.Console;
-Write ("Enter the total money: ");
-if (!int.TryParse (ReadLine (), out int money) || money <= 0)
-   Write ("Enter valid money");
-else {
-   Write ("Enter price of a chocolate: ");
-   if (!int.TryParse (ReadLine (), out int priceofChocolate) || priceofChocolate <= 0)
-      Write ("Enter valid price for a chocolate");
-   else {
-      Write ("Enter the number of wrappers in exchange for a chocolate: ");
-      if (!int.TryParse (ReadLine (), out int wrappers) || wrappers <= 0)
-         Write ("Enter valid number of wrappers");
-      else if (money >= priceofChocolate) {
-         var result = GetmaxChoc (money, priceofChocolate, wrappers);
-         Write ($"\nThe maximum number of chocolates :{result.MaxChoc}\nThe remaining money: {result.Money}\nThe remaining wrappers: {result.Wrappers}\n");
-      } else Write ("The Money is too Low.");
+while (true) {
+   Write ("Enter the total money: ");
+   if (!int.TryParse (ReadLine (), out int money) || money <= 0) {
+      Write ("Enter valid money\n");
+      continue;
+   } else {
+      while (true) {
+         Write ("Enter price of a chocolate: ");
+         if (!int.TryParse (ReadLine (), out int priceofChocolate) || priceofChocolate <= 0) {
+            Write ("Enter valid price for a chocolate\n");
+            continue;
+
+         } else {
+            while (true) {
+               Write ("Enter the number of wrappers in exchange for a chocolate: ");
+               if (!int.TryParse (ReadLine (), out int wrappers) || wrappers <= 0) {
+                  Write ("Enter valid number of wrappers\n");
+                  continue;
+               } else if (money >= priceofChocolate) {
+                  var result = GetmaxChoc (money, priceofChocolate, wrappers);
+                  Write ($"\nThe maximum number of chocolates :{result.MaxChoc}\nThe remaining money: {result.Money}\nThe remaining wrappers: {result.Wrappers}\n");
+                  break;
+               } else {
+                  Write ("The Money is too Low.");
+                  break;
+               }
+            }
+         }
+         break;
+      }
    }
+   break;
 }
 /// <summary>This method returns a tuple with maximum chocolate for the given money with remaining money and remaining wrappers</summary>
 /// <param name="a">This is the money available</param>
