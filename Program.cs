61e75583620086f9ab07d7015e29730c3d6db4a6
Shakthy <shakthy.r@metamation.com>
2023-09-28 15:38:11 +0530

    T23:Voting contest modified

--- Program.cs
+++ Program.cs
@@ -4,16 +4,21 @@
 //Sample Input :Helloworld
 //Sample Output :L, 3 => Winner and votes 
 using static System.Console;
-WriteLine ("Give your vote(s): ");
-string votes = ReadLine ().ToLower ().Replace (" ", "");
-if (!string.IsNullOrEmpty (votes) && votes.All (Char.IsLetter)) {
-   var vote = GetWinner (votes);
-   WriteLine ($"The Winner is {vote.Candidate} with {vote.Votes} vote(s)");
-} else Write ("Enter a valid vote");
+while (true) {
+   Write ("Give your vote(s): ");
+   string votes = ReadLine ().ToLower ().Replace (" ", "");
+   if (!string.IsNullOrEmpty (votes) && votes.All (Char.IsLetter)) {
+      var (c, v) = GetWinner (votes);
+      WriteLine ($"The Winner is {c} with {v} vote(s)");
+      break;
+   } else Write ("Enter a valid vote\n");
+}
 
 ///<summary>Function getWinner to return the winner with number of votes </summary>
 ///<param name="votes">votes given as string</param>
-(char Candidate, int Votes) GetWinner (string votes) {
-   var vote = votes.GroupBy (x => x).Select (x => (candidate: x.Key, votes: x.Count ())).OrderByDescending (x => x.votes).FirstOrDefault ();
-   return (vote.candidate, vote.votes);
-}
+(char Candidate, int Votes) GetWinner (string votes)
+   => votes.GroupBy (x => x)
+          .Select (x => (candidate: x.Key, votes: x.Count ()))
+          .OrderByDescending (x => x.votes)
+          .FirstOrDefault ();
+
