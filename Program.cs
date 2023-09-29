2ce2200f7368767561c65a16ad6befd841a27905
Shakthy <shakthy.r@metamation.com>
2023-09-29 10:17:48 +0530

    T24:Chocolate Wrappers(Modified)

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@
 // Sample Output :C = 4, X = 3, W = 1
 using static System.Console;
 while (true) {
-   Write ("Enter the total money: ");
+   Write ("Enter the the amount to buy chocolates: ");
    if (!int.TryParse (ReadLine (), out int money) || money <= 0) {
       Write ("Enter valid money\n");
       continue;
    } else {
       while (true) {
-         Write ("Enter price of a chocolate: ");
+         Write ("Enter the price of a chocolate: ");
          if (!int.TryParse (ReadLine (), out int priceofChocolate) || priceofChocolate <= 0) {
             Write ("Enter valid price for a chocolate\n");
             continue;
@@ -24,8 +24,8 @@ while (true) {
                   Write ("Enter valid number of wrappers\n");
                   continue;
                } else if (money >= priceofChocolate) {
-                  var result = GetmaxChoc (money, priceofChocolate, wrappers);
-                  Write ($"\nThe maximum number of chocolates :{result.MaxChoc}\nThe remaining money: {result.Money}\nThe remaining wrappers: {result.Wrappers}\n");
+                  var result = GetMaxChoc (money, priceofChocolate, wrappers);
+                  Write ($"\nTotal number of chocolates purchased and obtained in exchange for wrappers: {result.MaxChoc}\nThe amount remaining after the purchase: {result.Money}\nThe remaining wrappers: {result.Wrappers}\n");
                   break;
                } else {
                   Write ("The Money is too Low.");
@@ -42,7 +42,7 @@ while (true) {
 /// <param name="a">This is the money available</param>
 /// <param name="b">This is the price of a chocolate</param>
 /// <param name="c">This is the number of wrappers exchanges for a chocolate</param>
-(int MaxChoc, int Money, int Wrappers) GetmaxChoc (int a, int b, int c) {
+(int MaxChoc, int Money, int Wrappers) GetMaxChoc (int a, int b, int c) {
    int maxchoc = a / b, money = a % b, wrappers = maxchoc, extrachoc = 0;
    while (wrappers >= c) {
       for (; wrappers >= c; wrappers -= c) {
