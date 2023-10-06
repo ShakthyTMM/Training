ff66f2d0c8a9ac96ce94447e69f105060f8d9800
Shakthy <shakthy.r@metamation.com>
2023-10-06 12:56:51 +0530

    T28: Queue Implementation (Modified)

--- Program.cs
+++ Program.cs
@@ -11,11 +11,9 @@ queue.Display ();
 Console.WriteLine (queue.Peek ());
 queue.Display ();
 
-/// <summary> Class TQueue is to implement queue with array as the underlying data structure </summary>
-/// <typeparam name="T"></typeparam>
+/// <summary>Class TQueue is to implement queue with array as the underlying data structure</summary>
+/// <typeparam name="T">Datatype of the queue</typeparam>
 class TQueue<T> {
-   int mFront, mRear;
-   T[] mArray;
 
    /// <summary> Constructor of TQueue </summary>
    public TQueue () {
@@ -24,25 +22,18 @@ class TQueue<T> {
       mArray = new T[4];
    }
 
-   /// <summary> Property with front of the queue </summary>
-   public int Front => mFront;
-
-   /// <summary> Property with rear of the queue </summary>
-   public int Rear => mRear;
-
-   /// <summary> Property with capacity of the queue </summary>
+   /// <summary>Property with capacity of the queue</summary>
    public int Capacity => mArray.Length;
 
-   /// <summary> The method to add elements to the queue </summary>
-   /// <param name="a"> The value of the element to be added</param>
+   /// <summary>The method to add elements to the queue</summary>
+   /// <param name="a">The value of the element to be added</param>
    public void Enqueue (T a) {
       if (mRear == (Capacity - 1)) Array.Resize (ref mArray, Capacity * 2);
       mArray[++mRear] = a;
-
    }
 
-   /// <summary> The method to delete a element from the queue </summary>
-   /// <returns> Returns the element to be deleted </returns>
+   /// <summary>The method to delete a element from the queue</summary>
+   /// <returns>Returns the element to be deleted</returns>
    /// <exception cref="InvalidOperationException"></exception>
    public T Dequeue () {
       if (IsEmpty) throw new InvalidOperationException ();
@@ -53,21 +44,23 @@ class TQueue<T> {
       return item;
    }
 
-   /// <summary> The method to return the first element added to the queue </summary>
-   /// <returns> Returns the first element of the queue </returns>
+   /// <summary>The method to return the first element added to the queue</summary>
+   /// <returns>Returns the first element of the queue</returns>
    /// <exception cref="InvalidOperationException"></exception>
    public T Peek () {
       if (IsEmpty) throw new InvalidOperationException ();
       return mArray[mFront];
-
    }
 
-   /// <summary> IsEmpty Property to check whether the queue is empty </summary>
-   public bool IsEmpty => (mFront > mRear) ? true : false;
+   /// <summary>IsEmpty Property to check whether the queue is empty</summary>
+   public bool IsEmpty => mFront > mRear;
 
-   /// <summary> The method to display the elements of the queue </summary>
+   /// <summary>The method to display the elements of the queue</summary>
    public void Display () {
       for (int i = 0; i <= mRear; i++) Console.Write (mArray[i] + " ");
       Console.WriteLine ();
    }
+
+   int mFront, mRear;
+   T[] mArray;
 }
