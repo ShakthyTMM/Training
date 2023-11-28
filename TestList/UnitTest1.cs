e979b44752c21a8858456b28e42a6139ccab1114
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2023-11-28 12:38:07 +0530

    T26: Review changes

--- TestList/UnitTest1.cs
+++ TestList/UnitTest1.cs
@@ -7,72 +7,90 @@ namespace TestList {
       ///<summary>To test the Add method's functionality</summary>
       [TestMethod]
       public void TestAdd () {
-         for (int i = 1; i <= 3; i++)
+         for (int i = 1; i <= 3; i++) {
+            myList.Add (i);
             mList.Add (i);
-         Assert.AreEqual (3, mList.Count);
+         }
+         Assert.AreEqual (mList.Count, myList.Count);
+         myList.Add (4);
          mList.Add (4);
-         Assert.AreEqual (4, mList.Capacity);
+         Assert.AreEqual (mList.Capacity, myList.Capacity);
+         myList.Add (5);
          mList.Add (5);
-         Assert.AreEqual (8, mList.Capacity);
-         Assert.AreEqual (2, mList[1]);
+         Assert.AreEqual (mList.Capacity, myList.Capacity);
+         Assert.AreEqual (mList[1], myList[1]);
+         myList[3] = 10;
          mList[3] = 10;
-         Assert.AreEqual (10, mList[3]);
-         Assert.ThrowsException<IndexOutOfRangeException> (() => mList[10]);
-         Assert.ThrowsException<IndexOutOfRangeException> (() => mList[10] = 4);
+         Assert.AreEqual (mList[3], myList[3]);
+         Assert.ThrowsException<IndexOutOfRangeException> (() => myList[10]);
+         Assert.ThrowsException<IndexOutOfRangeException> (() => myList[10] = 4);
       }
 
       ///<summary>To test the Remove method's functionality</summary>
       [TestMethod]
       public void TestRemove () {
-         for (int i = 1; i <= 3; i++)
+         for (int i = 1; i <= 3; i++) {
+            myList.Add (i);
             mList.Add (i);
-         Assert.IsTrue (mList.Remove (3));
-         Assert.AreEqual (2, mList.Count);
-         Assert.AreEqual (4, mList.Capacity);
-         for (int i = 5; i <= 7; i++)
+         }
+         Assert.AreEqual (mList.Remove (3), myList.Remove (3));
+         Assert.AreEqual (mList.Count, myList.Count);
+         Assert.AreEqual (mList.Capacity, myList.Capacity);
+         for (int i = 5; i <= 7; i++) {
+            myList.Add (i);
             mList.Add (i);
-         Assert.AreEqual (8, mList.Capacity);
-         Assert.IsFalse (mList.Remove (-5));
+         }
+         Assert.AreEqual (mList.Capacity, myList.Capacity);
+         Assert.AreEqual (mList.Remove (-5), myList.Remove (-5));
       }
 
       ///<summary>To test the RemoveAt method's functionality</summary>
       [TestMethod]
       public void TestRemoveAt () {
-         for (int i = 1; i <= 3; i++)
+         for (int i = 1; i <= 3; i++) {
+            myList.Add (i);
             mList.Add (i);
+         }
+         myList.RemoveAt (2);
          mList.RemoveAt (2);
-         Assert.AreEqual (2, mList.Count);
-         Assert.ThrowsException<InvalidOperationException> (() => mList.RemoveAt (10));
+         Assert.AreEqual (mList.Count, myList.Count);
+         Assert.ThrowsException<InvalidOperationException> (() => myList.RemoveAt (10));
       }
 
       ///<summary>To test the Insert method's functionality</summary>
       [TestMethod]
       public void TestInsert () {
-         for (int i = 1; i <= 4; i++)
+         for (int i = 1; i <= 4; i++) {
+            myList.Add (i);
             mList.Add (i);
+         }
+         myList.Insert (1, 5);
          mList.Insert (1, 5);
-         Assert.AreEqual (5, mList.Count);
-         Assert.ThrowsException<InvalidOperationException> (() => mList.Insert (10, 5));
-         Assert.AreEqual (8, mList.Capacity);
+         Assert.AreEqual (mList.Count, myList.Count);
+         Assert.ThrowsException<InvalidOperationException> (() => myList.Insert (10, 5));
+         Assert.AreEqual (mList.Capacity, myList.Capacity);
       }
 
       ///<summary>To test the Clear method's functionality</summary>
       [TestMethod]
       public void TestClear () {
+         myList.Add (1);
          mList.Add (1);
+         myList.Clear ();
          mList.Clear ();
-         Assert.AreEqual (0, mList.Count);
-         Assert.ThrowsException<InvalidOperationException> (mList.Clear);
+         Assert.AreEqual (mList.Count, myList.Count);
+         Assert.ThrowsException<InvalidOperationException> (myList.Clear);
       }
 
       ///<summary>To test the Display method's functionality</summary>
       [TestMethod]
       public void TestDisplay () {
-         Assert.ThrowsException<InvalidOperationException> (mList.Display);
-         mList.Add (1);
-         mList.Display ();
+         Assert.ThrowsException<InvalidOperationException> (myList.Display);
+         myList.Add (1);
+         myList.Display ();
       }
 
-      MyList<int> mList = new ();
+      MyList<int> myList = new ();
+      List<int> mList = new ();
    }
 }
