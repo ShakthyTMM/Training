86e85a0be67c1ab50291064820defafd703f6386
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2023-12-18 15:09:36 +0530

    A10.1: File name parser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Training 
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------------------
+// Program.cs
+// File name parser
+// To parse a file name and return a tuple with the drive, folders, file name and extension
+// ----------------------------------------------------------------------------------------
+using static State;
+Console.Write ("Enter the file name: ");
+string input = Console.ReadLine ().Trim ().ToUpper () + "~";
+(char drive, string[] folder, string fname, string ext) = FileParse (input);
+Console.Write ($"Drive    : {drive}\nFolders  : ");
+foreach (var ch in folder) Console.Write ($"{ch}   ");
+Console.WriteLine ($"\nFile Name: {fname}\nExtension: {ext}");
+
+(char, string[], string, string) FileParse (string input) {
+   State s = A;
+   Action none = () => { };
+   Action todo;
+   char drive = ' '; int i = 0; string[] folder = new string[input.Length]; string fname = "", ext = "";
+   foreach (var ch in input) {
+      (s, todo) = (s, ch) switch {
+         (A, >= 'A' and <= 'Z') => (B, () => drive = ch),
+         (B, ':') => (C, none),
+         (C, '\\') => (D, none),
+         (D or E, >= 'A' and <= 'Z') => (E, () => folder[i] += ch),
+         (E, '\\') => (D, () => i++),
+         (E, '.') => (F, () => { fname = folder[i]; folder = folder[..i]; }),
+         (F or G, >= 'A' and <= 'Z') => (G, () => ext += ch),
+         (G, '~') => (I, none),
+         _ => (Z, none),
+      };
+      todo ();
+   }
+   if (s == I) return (drive, folder, fname, ext);
+   throw new FormatException ("Invalid format");
+
+}
+enum State { A, B, C, D, E, F, G, I, Z };
