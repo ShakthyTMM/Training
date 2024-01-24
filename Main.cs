13e5549d54369184af607a11ac31ad4fa1cc6111
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-01-24 13:03:48 +0530

    E2: Complex Numbers

---
+++ Main.cs
@@ -0,0 +1,6 @@
+﻿ComplexNumber num1 = new ComplexNumber (2, 7);
+ComplexNumber num2 = new ComplexNumber (3, 5);
+
+num1.Addition (num2);
+num1.Subtraction (num2);
+Console.WriteLine ($"Magnitude: {num1.norm}");
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿class ComplexNumber {
+   public ComplexNumber (int real, int imag) {
+      r = real;
+      i = imag;
+   }
+   public int Real => r;
+   public int Imag => i;
+
+   public double norm => Math.Sqrt (Math.Pow (r, 2) + Math.Pow (i, 2));
+
+   public void Addition (ComplexNumber num) {
+      Console.WriteLine ($"Addition:  { r + num.r}");
+   }
+   public void Subtraction (ComplexNumber num) {
+      Console.WriteLine ($"Subtraction: { i - num.i}");
+   }
+   readonly int r;
+   readonly int i;
+}
