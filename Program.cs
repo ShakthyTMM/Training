9d056979b3fe16f101a5c4c3044b45ec44ec120d
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-01-24 13:06:10 +0530

    E3: Tic Tac

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿using System.Text;
+using static System.Console;
+OutputEncoding = new UnicodeEncoding ();
+string[,] numbers = new string[,] { { "7", "8", "9" }, { "4", "5", "6" }, { "1", "2", "3" } };
+int x, y;
+(x, y) = GetCursorPosition ();
+Display ();
+void Display () {
+   CursorLeft = x;
+   CursorTop = y;
+   for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+         Write ($" {numbers[i, j]}  ");
+         if (j != 2) Write ("│");
+      }
+      WriteLine ();
+      if (i != 2) {
+         Write (string.Concat (Enumerable.Repeat ("────┼", 2)));
+         WriteLine ("────");
+      }
+   }
+}
+for (int k = 1; k <= 9; k++) {
+   var key = ReadKey (true);
+   for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+         numbers[i, j] = "D" + numbers[i, j];
+         if (key.Key.ToString () != numbers[i, j]) continue;
+         numbers[i, j] = "X";
+      }
+   }
+   Display ();
+}
+
