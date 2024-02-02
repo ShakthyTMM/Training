3c9cb1121582d8abc2517b3c8965d868fea5bf1e
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-02-02 15:32:04 +0530

    A14.1: Anagrams

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿// ----------------------------------------------------------------------------------------
+// Training 
+// Copyright (c) Metamation India.
+// ----------------------------------------------------------------------------------------
+// Program.cs
+// Anagram
+// To print all possible anagrams 
+// ----------------------------------------------------------------------------------------
+var wordsList = File.ReadAllLines ("C:/etc/words.txt")
+            .GroupBy (x => string.Concat (x.OrderBy (c => c))) // Grouping the anagrams by sorting them 
+            .Where (x => x.Count () > 1) // Eliminating the non anagrams words
+            .OrderByDescending (x => x.Count ()) // Arranging based on the number of anagrams in descending order 
+            .ToList ();
+using (StreamWriter sw = new ("C:/etc/test.txt"))
+   foreach (var words in wordsList) sw.WriteLine ($"{words.Count ()} {string.Join (", ", words)}");
