dc2d4e3f7b57c111f46a6f3b995e785899a2df5d
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-02-02 16:48:52 +0530

    A14.1: Removed ToList

--- Program.cs
+++ Program.cs
@@ -9,7 +9,6 @@
 var wordsList = File.ReadAllLines ("C:/etc/words.txt")
             .GroupBy (x => string.Concat (x.OrderBy (c => c))) // Grouping the anagrams by sorting them 
             .Where (x => x.Count () > 1) // Eliminating the non anagrams words
-            .OrderByDescending (x => x.Count ()) // Arranging based on the number of anagrams in descending order 
-            .ToList ();
+            .OrderByDescending (x => x.Count ()); // Arranging based on the number of anagrams in descending order 
 using (StreamWriter sw = new ("C:/etc/test.txt"))
    foreach (var words in wordsList) sw.WriteLine ($"{words.Count ()} {string.Join (", ", words)}");
