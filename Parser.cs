e88463cf96dde0348809efcc23162a00e1340a1a
ShakthyTMM <shakthy.ranganathan@trumpf.com>
2024-05-29 15:07:03 +0530

    A17.1 & A17.2: Implemented 'ExprGrapher' class and supported equality and comparison operators

---
+++ Parser.cs
@@ -0,0 +1,88 @@
+﻿namespace PSI;
+using static Token.E;
+
+class Parser {
+   public Parser (Tokenizer tokenizer)
+      => mToken = mPrevious = (mTokenizer = tokenizer).Next ();
+   Tokenizer mTokenizer;
+   Token mToken, mPrevious;
+
+   public NExpr Parse () => Expression ();
+
+   // Implementation --------------------------------------
+   // expression = equality
+   NExpr Expression () => Equality ();
+
+   // equality = comparison[("=" | "<>") comparison] .
+   NExpr Equality () {
+      var expr = Comparison ();
+      if (Match (EQ, NEQ)) {
+         var op = mPrevious;
+         expr = new NBinary (expr, op, Comparison ());
+      }
+      return expr;
+   }
+
+   // comparison = term[("<" | "<=" | ">" | ">=") term] .
+   NExpr Comparison () {
+      var expr = Term ();
+      if (Match (GT, GEQ, LT, LEQ)) {
+         var op = mPrevious;
+         expr = new NBinary (expr, op, Term ());
+      }
+      return expr;
+   }
+
+   // term = factor { ("+" | "-") factor } .
+   NExpr Term () {
+      var expr = Factor ();
+      while (Match (ADD, SUB)) {
+         var op = mPrevious;
+         expr = new NBinary (expr, op, Factor ());
+      }
+      return expr;
+   }
+
+   // factor = unary { ("*" | "/") unary } .
+   NExpr Factor () {
+      var expr = Unary ();
+      while (Match (MUL, DIV)) {
+         var op = mPrevious;
+         expr = new NBinary (expr, op, Unary ());
+      }
+      return expr;
+   }
+
+   // unary = ( "-" | "+" ) unary | primary .
+   NExpr Unary () {
+      if (Match (ADD, SUB))
+         return new NUnary (mPrevious, Unary ());
+      return Primary ();
+   }
+
+   // primary = IDENTIFIER | INTEGER | REAL | STRING | "(" expression ")" .
+   NExpr Primary () {
+      if (Match (IDENT)) return new NIdentifier (mPrevious);
+      if (Match (INTEGER, REAL, STRING)) return new NLiteral (mPrevious);
+      Expect (OPEN, "Expecting identifier or literal");
+      var expr = Expression ();
+      Expect (CLOSE, "Expecting ')'");
+      return expr;
+   }
+
+   // Helpers ---------------------------------------------
+   // Expect to find a particular token
+   void Expect (Token.E kind, string message) {
+      if (!Match (kind)) throw new Exception (message);
+   }
+
+   // Match and consume a token on match
+   bool Match (params Token.E[] kinds) {
+      if (kinds.Contains (mToken.Kind)) {
+         mPrevious = mToken;
+         mToken = mTokenizer.Next ();
+         return true;
+      }
+      return false;
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace PSI;
+
+static class Start {
+   static void Main () {
+      Routine ("(3 + 2) * 4 - 17 * -five * (two + 1 + 4 + 5)");
+      Routine ("3 + 2 > (6 * 5)");
+      Routine ("a = (5 + 5) - 6 + 2");
+   }
+
+   // Checks the 'ExprGrapher' visitor pattern 
+   static void Routine (string expr) {
+      var parser = new Parser (new Tokenizer (expr));
+      var node = parser.Parse ();
+      var sb = node.Accept (new ExprILGen ());
+      Console.WriteLine ("\nGenerated code: ");
+      Console.WriteLine (sb);
+      var graph = new ExprGrapher (expr);
+      node.Accept (graph);
+      Directory.CreateDirectory ("c:/etc");
+      graph.SaveTo ("c:/etc/test.html");
+      var pi = new ProcessStartInfo ("c:/etc/test.html") { UseShellExecute = true };
+      Process.Start (pi);
+      Console.Write ("\nPress any key..."); Console.ReadKey (true);
+   }
+}
